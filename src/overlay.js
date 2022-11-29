32b0223b2b26bc66f54f32e3a2b8a96cbc71babf
vanjuxin-dju <sokol.vdu@gmail.com>
2022-11-29 20:09:05 +0300

    removed hover state of overlay divs, moved handlers to arrows, added examples of opening a presentation from a specific slide

--- src/overlay.js
+++ src/overlay.js
@@ -2,21 +2,21 @@ export default class Overlay {
     constructor(slidesDirection) {
         this.overlayElement = document.createElement('div');
         this.overlayElement.classList.add("overlay", "overlay-" + slidesDirection);
-        this.overlayElement.innerHTML = `<div class="previous-slide" name="previous" title="Previous slide">
-                                            <div class="arrow"></div>
+        this.overlayElement.innerHTML = `<div class="previous-slide" name="previous">
+                                            <div class="arrow" title="Previous slide"></div>
                                         </div>
-                                        <div class="next-slide" name="next" title="Next slide">
-                                            <div class="arrow"></div>
+                                        <div class="next-slide" name="next">
+                                            <div class="arrow" title="Next slide"></div>
                                         </div>`;
         document.body.append(this.overlayElement);
     }
 
     addNextSlideClickListener(callback) {
-        this.overlayElement.children.next.onclick = callback;
+        this.overlayElement.children.next.children[0].onclick = callback;
     }
 
     addPreviousSlideClickListener(callback) {
-        this.overlayElement.children.previous.onclick = callback;
+        this.overlayElement.children.previous.children[0].onclick = callback;
     }
 
     disablePreviousButton() {
